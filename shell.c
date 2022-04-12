94337b58262c2c6dd5632f6d92dab195a183e778
taret92 <ivancho92.10@hotmail.com>
2022-04-11 23:24:40 -0500

    Starting the repo

---
+++ shell.c
@@ -0,0 +1,62 @@
+#include "main.h"
+/**
+ * 
+ * @param argc 
+ * @param argv 
+ * @param env 
+ * Return: 0
+ */
+
+int main(int argc, char **argv, char **env)
+{
+	UNUSED(argc);
+	UNUSED(argv);
+	UNUSED(env);
+	{
+	int read;
+	int tty = 1;
+	size_t len = 0;
+	char *line, *cpy, *arg;
+	args_t *arguments;
+	
+
+	isatty(STDIN_FILENO) == 0 ? tty = 0 : tty;
+
+	do{
+		tty == 1 ? write(STDOUT_FILENO, "($) ", 4) : 0;
+		fflush(stdin);
+		read = getline(&line, &len, stdin);
+		if(read == -1)
+		{
+			return(-1);
+		}
+		cpy = strdup(line);
+
+		for (; (arg = strtok(cpy, " \t\n")); cpy = NULL)
+		{
+			if(arg == NULL)
+			{
+				break;
+			}
+			add(&arguments, arg);
+		}
+
+		size_t print(args_t *head)
+	{
+
+		int i;
+
+		for(i = 0; head; i++)
+		{
+			printf("argument -> [%s]", head->arg);
+
+		}
+		return (i);
+	}
+
+
+	}while(1);
+
+	return(0);
+}
+}
